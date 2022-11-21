a94e6076b0489a9f40c834b351269fbd86ed3a72
kylee <kyleedevzero86@gmail.com>
2022-11-21 12:12:25 +0900

    주석추가

---
+++ VOD/src/main/java/com/thrj/Entity/Movies.java
@@ -0,0 +1,31 @@
+package com.thrj.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // getter setter
+@ToString
+public class Movies {
+	
+	@NonNull private int movie_seq;                    // 영화순번
+	@NonNull private String movie_title;               // 영화제목
+	@NonNull private String movie_content;             // 영화 줄거리
+	@NonNull private String movie_img;                 // 영화이미지
+	@NonNull private String movie_type;                // 영화줄거리
+	@NonNull private String movie_open_date;           // 영화개봉날짜           
+	@NonNull private String movie_rating;              // 영화점수
+	@NonNull private String movie_runtime;             // 상영시간
+	@NonNull private String movie_actor;               // 영화 출연배우
+	@NonNull private String movie_country;             // 영화 제작국가
+	@NonNull private String admin_id;                  // 관리자 아이디
+	@NonNull private String movie_director;            // 영화감독
+	private String movie_age;                          // 영화 시청연령
+	
+}
