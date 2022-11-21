316c762c41121866df628bf72979ce1d78a3762d
kylee <kyleedevzero86@gmail.com>
2022-11-21 13:27:03 +0900

    테스트

--- VOD/src/main/java/com/thrj/vod/MovieController.java
+++ VOD/src/main/java/com/thrj/vod/MovieController.java
@@ -1,13 +1,14 @@
 package com.thrj.vod;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.thrj.Entity.Members;
+import com.thrj.Entity.Movies;
 import com.thrj.Mapper.MovieMapper;
 
 @Controller
@@ -17,7 +18,9 @@ public class MovieController {
 	public MovieMapper mapper;
 	
 	@GetMapping(value={"/index.do","/"})
-	public String index() {
+	public String index(Model model) {
+		List<Movies> list = mapper.movieList();
+		model.addAttribute("list",list);
 		return "index";
 	}
 	
@@ -46,17 +49,10 @@ public class MovieController {
 	}
 	
 	@GetMapping("/categories.do")
-	public String categories() {
-		
+	public String categories(Model model) {
+		List<Movies> list = mapper.movieList();
+		model.addAttribute("list",list);
 		return "categories";
-	}
-	
-	//	Test
-	@GetMapping("/test.do")
-	public String test(Model model) {
-		Members vo = mapper.idCheck("mb_id 001");
-		model.addAttribute("vo", vo);
-		return "test";
 	}
 	
 }
