70f94c6f7dfffcc43b52bd95fe4a5ec5dc9dd80b
kylee <kyleedevzero86@gmail.com>
2022-11-22 18:55:42 +0900

    api제거

--- VOD/src/main/webapp/resources/js/main.js
+++ VOD/src/main/webapp/resources/js/main.js
@@ -9,67 +9,6 @@
 
 //이미지를 처음 불러올때 처리하는기능
 
-var api_key = "9d58f50eea3bc0cebe233c422bbef69e";
-var base_url = "https://image.tmdb.org/t/p/w500/";
-var key= "f9a5e15b3b0e1e90e656e39bf714f0a3";
-var yesterday = ( d => new Date(d.setDate(d.getDate() -1)))(new Date); //어제 날짜 구하기
-yesterday = yesterday.getFullYear()+""+(yesterday.getMonth()+1)+""+ yesterday.getDate();
-var date = yesterday; //어제 날짜를 넣어 준다.
-var apiUrl = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key="+key+"&targetDt="+date;
-var m_title="";
-window.addEventListener('load', () => {
-    const xhr = new XMLHttpRequest(); //리퀘스트 객체를 만든다.
-    xhr.onreadystatechange = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){
-            var jsonObj = JSON.parse(xhr.response); 
-            var boxOfficeResult = jsonObj['boxOfficeResult'];
-			var movieList = boxOfficeResult['dailyBoxOfficeList'];
-            var searchUrl = "https://api.themoviedb.org/3/search/movie?api_key="+api_key+"&language=ko-Kr&page=1&include_adult=false&query="+movieList[0].movieNm;
-            var searchUrl2 = "https://api.themoviedb.org/3/genre/movie/list?api_key="+api_key+"&language=ko";
-            
-            fetch(searchUrl)
-            .then(res => res.json())
-            .then(function(res){
-        
-            	var overview=res.results[0].overview; //줄거리
-            	var backdrop_path=res.results[0].backdrop_path; //이미지경로
-            	m_title=res.results[0].title; //영화제목
-            	var genre_ids=res.results[0].genre_ids; //장르코드
-            	
-            	 fetch(searchUrl2)
-                 .then(res2 => res2.json())
-                 .then(function(res2){
-                	 if(genre_ids[0]==res2.genres[0].id){
-                	 
-                	 	$('.movie_title').each(function() { //타이틀명
-                	 		$(this).append("<h2 class='movie_title'>"+m_title+"</h2>");
-                	 	});
-                	 	
-                	 	$('.movie_content').each(function() { //줄거리요약
-                	 		$(this).css({'line-height':'1.2em','height':'1.6em'});
-                	 		$(this).append("<p class='movie_content'>"+(overview.substr(0,38)+' ....')+"</p>");
-                	 		
-                	 	});
-                	 	
-                	 	$('.label').each(function() { //장르 코드명
-                	 		$(this).append("<div class='label'>"+res2.genres[0].name+"</div>");
-                	 	});
-                	 	/*
-                	 	$('.hero__items').each(function() { //배경이미지
-       						var bg = $(this).data('setbg');
-       						$(this).css('background-image', 'url(' + base_url+backdrop_path + ')');
-    					});*/
-    							
-    				 }
-                 }).catch(error => console.log(error));
-            }).catch(error => console.log(error));
-        }
-    }
-    xhr.open('GET', apiUrl, true);
-    xhr.send();
-});
-
-
 'use strict';
 
 (function ($) {
