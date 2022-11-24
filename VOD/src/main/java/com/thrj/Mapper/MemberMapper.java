7b50d837f056dbaba686716f2716b0ea22223d47
kylee <leekydevzro86@gmail.com>
2022-11-24 15:28:29 +0900

    로그인 2022.11.18

--- VOD/src/main/java/com/thrj/Mapper/MemberMapper.java
+++ VOD/src/main/java/com/thrj/Mapper/MemberMapper.java
@@ -1,8 +1,11 @@
 package com.thrj.Mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.thrj.Entity.Members;
+import com.thrj.Entity.Movies;
 
 @Mapper
 public interface MemberMapper {
@@ -11,5 +14,6 @@ public interface MemberMapper {
 	public void createMember(Members vo);   // 유저등록
 	public void updateMember(Members vo);   // 사용자수정
 	public void deleteMember(String mb_id); // 사용자삭제
-	public boolean logincheck(String mb_id, String mb_pw); //로그인체크
+	public Members logincheck(Members vo); //사용자 아이디 로그인 찾기
+	public Members retrieveSessionInfo(String mb_id);//사용자 상세정보 가져오기
 }
--- VOD/src/main/java/com/thrj/vod/MemberController.java
+++ VOD/src/main/java/com/thrj/vod/MemberController.java
@@ -1,6 +1,7 @@
 package com.thrj.vod;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.thrj.Entity.Members;
+import com.thrj.Entity.Movies;
 import com.thrj.Mapper.MemberMapper;
+import com.thrj.Mapper.MovieMapper;
 
 
 @Controller
@@ -28,96 +30,95 @@ public class MemberController {
 	@Autowired
 	public MemberMapper mapper;
 	
-	@PostMapping("/idCheck.do")
-	public @ResponseBody Members idCheck(String mb_id) {
-		
-		// Mapper.interface, Mapper.xml
-		// 해당 id가 존재하면 회원정보(id, pw, nick, addr)를 받아오고
-		// 해당 id가 존재하지 않으면 null
-		Members vo = mapper.idCheck(mb_id);
-		if(vo == null) {
-			vo = new Members();
-			vo.setMb_id("null");
-		}
-		
-		return vo;
-	}
+	@Autowired
+	public MovieMapper movie_mapper;
 	
 	@GetMapping("/login.do")
-	public String login(@ModelAttribute Members vo, HttpServletRequest request){
-		HttpSession session = request.getSession();
-
-		boolean checkVO = mapper.logincheck(vo.getMb_id(),vo.getMb_pw());
-		
-		//로그인체크
-		
-		session.setMaxInactiveInterval(-1); 
-		return "redirect:/index.do";
-	}
-	
-	@GetMapping("/logincheck.do")
-	@ResponseBody
-	public Map<String, String> ajaxLogincheck(HttpServletRequest request
-			, @RequestParam(value="id", required=false) String id
-			, @RequestParam(value="pw", required=false) String pw) {
-		
-		Map<String, String> loginYnMap = new HashMap<String, String>();
-		boolean check = false;
-		check = mapper.logincheck(id, pw);
-		if(check){
-			loginYnMap.put("loginYn", "success");
-		}else{
-			loginYnMap.put("loginYn", "fail");
-		}
-		return loginYnMap;
+	public ModelAndView login(){
+		ModelAndView mv = new ModelAndView();
+		Movies detail = movie_mapper.bannerOne();
+		mv.addObject("movies",detail);
+		mv.setViewName("login");
+		return mv;
 	}
 	
 	@RequestMapping(value="/logout.do")
 	public String logout(HttpServletRequest request){
 		HttpSession session = request.getSession();
-		session.removeAttribute("id");
-		session.removeAttribute("userId");
+		session.removeAttribute("mb_id");
 		session.invalidate();
 		return "redirect:/index.do";
-
+	}
+	
+	@RequestMapping(value="/login_ok.do",method = RequestMethod.POST)
+	public ModelAndView login_ok(@ModelAttribute Members vo, HttpServletRequest request){
+		ModelAndView mv = new ModelAndView();
+		HttpSession session = request.getSession();
+		Members userBean=mapper.logincheck(vo);
+		session.setAttribute("mb_id", userBean.getMb_id());
+		mv.setViewName("redirect:/index.do");
+		return mv;
+	}
+	
+	@RequestMapping(value="/ajaxLoginCheck.do", method = {RequestMethod.GET, RequestMethod.POST})
+	@ResponseBody
+	public Map<String, String> ajaxLogincheck(HttpServletRequest request
+			, @RequestParam(value="mb_id", required=false) String mb_id
+			, @RequestParam(value="mb_pw", required=false) String mb_pw) {
+		Map<String, String> loginYnMap = new HashMap<String, String>();
+		Members vo=new Members();
+		vo.setMb_id(mb_id);
+		vo.setMb_pw(mb_pw);
+		
+		Members userBean=mapper.logincheck(vo);
+		if(userBean!=null) { //아이디가 저장되었는지  확인여부
+			loginYnMap.put("loginYn", "success");
+		 } else {
+			loginYnMap.put("loginYn","fail");
+		}
+		
+		return loginYnMap;
+	}
+	
+	@RequestMapping(value="/deleteMember.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView deleteMember(HttpServletRequest request, @ModelAttribute Members vo){
+		ModelAndView mv = new ModelAndView();
+		 HttpSession session = request.getSession();
+		 Members userBean=(Members)session.getAttribute("mb_id");
+		 String mb_id = userBean.getMb_id();
+		 mapper.deleteMember(mb_id);
+		 mv.setViewName("redirect:/index.do");
+	  return mv;
+	}
+	
+	@RequestMapping(value="/myinfoMember.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView myinfoMember(HttpServletRequest request){
+		ModelAndView mv = new ModelAndView();
+		 HttpSession session = request.getSession();
+		 String movie_id=(String)session.getAttribute("mb_id");
+		 Members userBean=mapper.retrieveSessionInfo(movie_id);
+		 mv.addObject("userBean",userBean);
+		 mv.setViewName("memberInfo");
+	  return mv;
+	}
+	
+	@RequestMapping(value="/updateMember.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public ModelAndView updateMember(HttpServletRequest request, @ModelAttribute Members vo){
+			ModelAndView mv = new ModelAndView();
+			mapper.updateMember(vo);
+			mv.setViewName("redirect:/myinfoMember.do");
+		return mv;
 	}
 	
 	@RequestMapping(value="/signup.do", method = {RequestMethod.GET, RequestMethod.POST})
 	public ModelAndView createUser(@ModelAttribute Members vo) {	
 		ModelAndView mv = new ModelAndView();
-        String mb_id = vo.getMb_id(); 
-		Map<String, String> codeParam = new HashMap<String, String>();
-
-		if(mb_id == null){
-			//아이디없이  저장 버튼을 누를경우
-			mv.setViewName("/signup");
-		}else{
-
-			//아이디를 넣고 저장 버튼을 누를 경우
-			mv.setViewName("/signup");
-		}
+		Movies detail = movie_mapper.bannerOne();
+		mv.addObject("movies",detail);
+        mv.setViewName("/signup"); 
 		return mv;
 	}
 	
-	
-	@RequestMapping(value="/deleteMember.do", method = {RequestMethod.GET, RequestMethod.POST})
-	public void deleteUser(HttpServletRequest request, @ModelAttribute Members vo){
-		HttpSession session = request.getSession();
-		ModelAndView mv = new ModelAndView();
-    	String mb_id = (String) session.getAttribute("mb_id"); 
-		mapper.deleteMember(mb_id);;
-		mv.setViewName("redirect:/index.do");
-	}
-	
-	
-	@RequestMapping(value="/updateMember.do", method = {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView updateMember(HttpServletRequest request, @ModelAttribute Members vo){
-			HttpSession session = request.getSession();
-			ModelAndView mv = new ModelAndView();
-			mapper.updateMember(vo);
-			mv.setViewName("redirect:/index.do");
-		return mv;
-	}
 	
 	@RequestMapping(value="/member/saveFile.do", method=RequestMethod.POST)
 	@ResponseBody
@@ -125,12 +126,11 @@ public class MemberController {
 		//저장할 경로
 		//서버체크 (로컬 / 실제서버)
 		//단일파일업로드
-		String imgFolder =""; //저장할 경로
-		String realFolder = ""; //web-inf바로전 까지 저장할 경로
+		//String imgFolder =""; //저장할 경로
+		//String realFolder = ""; //web-inf바로전 까지 저장할 경로
 		//단일 파일경로
-		String filename = "";//실제파일경로
-
-		return filename;
+		//String filename = "";//실제파일경로
+		return null;
 	}
 
 }
