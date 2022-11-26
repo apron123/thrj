ba94c5615d19026eb76b0c0c52084c2cc6ad90fc
이경용 <sleekydz86@naver.com>
2022-11-26 15:55:12 +0900

    정리

--- VOD/src/main/java/com/thrj/Mapper/MovieMapper.java
+++ VOD/src/main/java/com/thrj/Mapper/MovieMapper.java
@@ -14,12 +14,12 @@ public interface MovieMapper {
 	public Movies animeDetails(int movie_seq); //박스오피스에서 번호값을 받았을경우
 	public void raiseLookupCount(int movie_seq);//게시물을 볼경우 count
 	public Movies bannerOne(); //하나만 사용할때
+	public void insertHistorySeq(Movies vo); // 상세페이지 장르별 유사 영화 삽입
+	public List<History> historySeq(String mb_id); //시청 이력 가져오기
 	
 	public List<Movies> bannerList();
 	public int getTotalRowCount(Paging paging);
 	public List<Paging> getPageList(Paging paging);
 	public void updateStarRating(Movies vo); // 유저 별점 남기기 
 	public List<Movies> movieGenreList(Movies vo); // 상세페이지 장르별 유사 영화 추천 
-	public void insertHistorySeq(Movies vo); // 상세페이지 장르별 유사 영화 삽입
-	public List<History> historySeq(String mb_id);
 }
