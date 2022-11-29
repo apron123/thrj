f5cddc823e0cea808089efaf678585bf7a50a8ab
이경용 <sleekydz86@naver.com>
2022-11-29 13:04:00 +0900

    삭제기능 수정

--- VOD/src/main/java/com/thrj/Mapper/CommentsMapper.java
+++ VOD/src/main/java/com/thrj/Mapper/CommentsMapper.java
@@ -12,4 +12,5 @@ public interface CommentsMapper {
 	public void ListComments(Comments vo);   //  댓글순서
 	public int CommentsCnt(int movieSeq);   //   카운트 수
 	public List<Comments> getAllCommentsByPage(int movieSeq); //리스트수
+	public void deleteComments(String mb_id); // 댓글삭제
 }
